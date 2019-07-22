df237d99730e633405c4d98f1b8a5581129a29ba
Vita Mileva <milayevavita27@gmail.com>
2019-07-22 12:21:58 +0300

    Updates

---
+++ precache-manifest.1ab321dcbcda672bc382f578ffe71dc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbdd0f3019e89f407351dc2e992ab364",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3862041aa226690eab84",
+    "url": "/portfolio/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/portfolio/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "3862041aa226690eab84",
+    "url": "/portfolio/static/js/main.83ce4be4.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.eb52d3f27ca3ad2593d0fcbe17f0353c.js"
+  "/portfolio/precache-manifest.1ab321dcbcda672bc382f578ffe71dc7.js"
 );
 
 self.addEventListener('message', (event) => {
