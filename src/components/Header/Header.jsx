e978bc3a73689f06d56bec3efc61f27cae03a2ca
Vita Mileva <milayevavita27@gmail.com>
2019-07-22 17:23:02 +0300

    add menu_apps

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import nav from './Header.module.css'
+import React from "react";
+import nav from "./Header.module.css";
 
 const Header = () => {
   return (
-  <header className={nav.header}>
-    <div className={nav.container}>
-      <div className={nav.phone}></div>
-      <div className={nav.menu}>
-        <a href="#" className={nav.top}>home</a>
-        <a href="#" className={nav.top}>about</a>
-        <a href="#" className={nav.top}>skills</a>
-        <a href="#" className={nav.top}>portfolio</a>
-
+    <header className={nav.header}>
+      <div className={nav.container}>
+        <div className={nav.cols}>
+          <div className={nav.col}>
+            <a href="tel:+38 (093) 650 4658" className={nav.tel}>
+              +38 (093) 650 46 58
+            </a>
+          </div>
+          <div className={nav.col}>
+            <a href="#" className={nav.top}>
+              home
+            </a>
+            <a href="#" className={nav.top}>
+              about
+            </a>
+            <a href="#" className={nav.top}>
+              skills
+            </a>
+            <a href="#" className={nav.top}>
+              portfolio
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  </header>
-  )
-}
+    </header>
+  );
+};
 export default Header;
