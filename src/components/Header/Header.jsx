64e7f7bf99c05971703ed07c296c97782c051c31
Vita Mileva <milayevavita27@gmail.com>
2019-07-23 00:08:18 +0300

    create summary

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,6 +11,7 @@ const Header = () => {
             <a href="tel:+38 (093) 650 4658" className={nav.tel}>
               +38 (093) 650 46 58
             </a>
+            <strong>MILAEVA VITALIIA</strong>
           </div>
           <div className={nav.col}>
             <a href="#" className={nav.top}>
@@ -25,6 +26,9 @@ const Header = () => {
             <a href="#" className={nav.top}>
               portfolio
             </a>
+            <a href="#" className={nav.top}>
+              contact
+            </a>
           </div>
         </div>
         <div className={nav.home}>
@@ -32,6 +36,7 @@ const Header = () => {
         </div>
         <div className={nav.block}>
           <h1 className={nav.title}>frontend developer</h1>
+          <p className={nav.text}>I want to start a Frontend Developer career in a large international company and in five years to grow to the team leader!</p>
         </div>      
       </div>
     </header>
