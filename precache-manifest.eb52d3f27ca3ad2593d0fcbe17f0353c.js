60ad58f586c8bd87ef236dceace53c7c8da5ea8f
Vita Mileva <milayevavita27@gmail.com>
2019-07-22 11:24:32 +0300

    Updates

---
+++ precache-manifest.eb52d3f27ca3ad2593d0fcbe17f0353c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0736eb00916714248f23a37310a121cd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "295c7de24526d11e0281",
+    "url": "/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "295c7de24526d11e0281",
+    "url": "/portfolio/static/js/main.55dc58d7.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
